0afaa2b0a7258b5f78b5e04785cdc087b3efcdd1
Hiroshi Naraki <naraki@gmail.com>
2019-11-09 10:21:39 +0900

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div id='map'></div>
+</template>
+<script>
+  export default {
+    mounted () {
+      mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+      //mapboxgl.accessToken = MAPBOX_TOKEN;
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: process.env.MAPBOX_STYLE,
+        center: [139.767125,35.681236],
+        zoom: 10
+      })
+    }
+  }
+</script>
+<style>
+body { margin:0; padding:0; }
+#map { position:absolute; top:0; bottom:0; width:100%; }
+</style>
